9397a25061839c2eec5b6332ea60e23f09290fdd
Kevin Eliezer <kevineli.gp@gmail.com>
2020-11-17 15:15:13 +0000

    Fix database and added Characters Get endpoint

--- src/api/models/character.go
+++ src/api/models/character.go
@@ -1,20 +1,94 @@
 package models
 
 import (
+	"errors"
+	"log"
 	"time"
+
+	common "github.com/fullstacktf/Narrativas-Backend/common"
 )
 
-// Character : structure
+// Character : character BBDD structure
 type Character struct {
-	ID        uint   `gorm:"primary_key" json:"id"`
-	UserID    uint   `gorm:"NOT NULL" json:"userId" binding:"required"`
-	Name      string `gorm:"type:varchar(50)" json:"name"`
-	Biography string `json:"biography"`
-	UpdatedAt time.Time
-	CreatedAt time.Time
+	ID          uint   `gorm:"primary_key" json:"id"`
+	UserID      uint   `gorm:"foreign_key; column:user_id" json:"userid"`
+	Name        string `gorm:"type:varchar(50)" json:"name" binding:"required"`
+	Description string `json:"description" binding:"required"`
+	Image       string `gorm:"type:varchar(150)" json:"image" binding:"required"`
+	UpdatedAt   time.Time
+	CreatedAt   time.Time
 }
+
+// CharacterData : character API structure
+type CharacterData struct {
+	Name        string `gorm:"type:varchar(50)" json:"name" binding:"required"`
+	Description string `json:"description" binding:"required"`
+	Image       string `gorm:"type:varchar(150)" json:"image" binding:"required"`
+}
+
+// Characters : array of characters
+type Characters []CharacterData
 
 // TableName : Database table name map
 func (Character) TableName() string {
-	return "character"
+	return "actor"
+}
+
+// Insert : Inserts character into bbdd
+func (character Character) Insert() (bool, error) {
+	// TODO: set ID depending the user
+	character.UserID = 1
+	character.CreatedAt = time.Now()
+	character.UpdatedAt = time.Now()
+
+	if len(character.Name) > 50 || len(character.Name) == 0 {
+		return false, errors.New("character name too long")
+	}
+
+	if result := common.DB.Omit("Id").Create(&character); result.Error != nil {
+		return false, errors.New("invalid data provided")
+	}
+	return true, nil
+}
+
+// Delete : Delete character from bbdd
+func (character Character) Delete() (bool, error) {
+
+	return true, nil
+}
+
+// Get : returns all characters of one user
+func (characters *Characters) Get(id int) error {
+	rows, err := common.DB.Debug().
+		Model(&Character{}).
+		Select(`actor.name,
+				    actor.description
+					`).
+		Joins("JOIN user on user.id = actor.user_id").
+		Where("user.id = ?", id).
+		Rows()
+
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		character := CharacterData{}
+		err = common.DB.ScanRows(rows, &character)
+		if err != nil {
+			log.Println("error al bindear", err)
+		} else {
+			log.Printf("Character:%v\n", character)
+		}
+		*characters = append([]CharacterData(*characters), character)
+	}
+	return nil
+}
+
+// Get : Returns all information about one character
+func (character Character) Get() (bool, error) {
+	// TODO
+	return true, nil
 }
